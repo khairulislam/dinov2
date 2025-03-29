e05f000d7cb5cc7acfafffe5cb8049812b79e347
Md Khairul Islam <khairulislamtanim@gmail.com>
2025-03-29 15:17:20 -0400

    do not enable dist if already enabled

--- dinov2/utils/config.py
+++ dinov2/utils/config.py
@@ -50,7 +50,7 @@ def default_setup(cfg):
     is_distributed = getattr(cfg.train, "is_distributed", True)
     
     # distributed but not on windows
-    if is_distributed and os.name != 'nt' :
+    if is_distributed and os.name != 'nt' and not distributed.is_initialized():
         distributed.enable(overwrite=True)
         
     seed = getattr(cfg.train, "seed", 42)
